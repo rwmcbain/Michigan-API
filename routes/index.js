d750f2853e0024bdade7d8e3537281aa6d93aaaa
Russell McBain <Russell@MacBook-Pro-2.local>
2016-12-12 13:38:57 -0500

    cleaned up

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Michigan State Parks' });
+  res.render('index', { title: 'Michigan Information Portal' });
 });
 
 module.exports = router;
